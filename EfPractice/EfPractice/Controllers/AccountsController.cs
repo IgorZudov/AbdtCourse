36dcc831fc2907c1699aea2c7f0620250c8a2105
zudov_iv <igor.zudov.prog@gmail.com>
2021-12-02 12:57:06 +0300

    add in memory and include

--- EfPractice/EfPractice/Controllers/AccountsController.cs
+++ EfPractice/EfPractice/Controllers/AccountsController.cs
@@ -60,5 +60,54 @@ namespace EfPractice.Controllers
             var accounts = await query.ToListAsync();
             return Ok(accounts);
         }
+        
+        [HttpGet("in-memory")]
+        public async Task<ActionResult> GetInMemory()
+        {
+            var accounts = await context.Accounts.Select(x => new
+            {
+                x.Currency,
+                Symbol = GetSymbol(x.Currency)
+            }).ToListAsync();
+            return Ok(accounts);
+        }
+        
+        private static string GetSymbol(string currency)
+        {
+            if (currency.ToLower().StartsWith("ru"))
+                return "P.";
+
+            return currency.ToLower() switch
+            {
+                "usd" => "$.",
+                "eur" => "Eu.",
+                _ => "P."
+            };
+        }
+        
+        [HttpGet("in-memory-filter")]
+        public async Task<ActionResult> FilterInMemory()
+        {
+            var accounts = context
+                .Accounts
+                .AsEnumerable()
+                .Where(x => GetSymbol(x.Currency) == "$.")
+                .ToList();
+               // .ToListAsync();
+            return Ok(accounts);
+        }
+        
+        [HttpGet("include")]
+        public async Task<ActionResult> GetWithInclude()
+        { 
+            var accounts = await context.Accounts
+                .Include(x => x.Card)
+                .Select(x => new
+            {
+                x.Currency,
+                CardNumber = x.Card.Number
+            }).ToListAsync();
+            return Ok(accounts);
+        }
     }
 }
